68f282114899b0f08fd9424e52969a7cbaa890e6
SebastianBuritica <93333842+SebastianBuritica@users.noreply.github.com>
2022-05-27 14:42:57 -0500

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,35 @@
-import './app.css';
-import CollectionCard from './components/CollectionCard';
+import './App.css';
 import Header from './components/Header';
-// import { useState, useEffect } from 'react'
-// import axios from 'axios'
-// import Punklist from './components/Punklist';
-
- 
+import './components/Header.css';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import AbstractList from './components/AbstractList';
+import Main from './components/Main';
 
 function App() {
-  
-
+  const [listData, setListData] = useState([])
+  const [selectedNft, setSelectedNft] = useState(0)
+  useEffect(()=>{
+    const getMyNfts=async()=>{
+      const openseaData = await axios.get(
+        'https://testnets-api.opensea.io/assets?asset_contract_address=0x864E013490eCaCd0ABf8c9F6E6e4C2C354817712&format=json&order_direction=asc'
+      )
+      setListData(openseaData.data.assets)
+    }
+    return getMyNfts()
+  }, [])
   return (
-    <>
-    
-    <div className="app">
-      < Header />
-      <CollectionCard 
-        id={0} 
-        name={'Gekko test'} 
-        traits={[{'value': 7}]} 
-        image='https://opensea.mypinata.cloud/ipfs/QmNosQUMy8CLTn4BnM95yVLWLTrEzZW3cMSFozsGrMDgTY/0.png' 
-      />
-      {/* < Punklist punkListData={punkListData} /> */}
-      
+    <div className='app'>
+  <Header/>
+  { listData.length > 0 && (
+  <>
+  <Main listData={listData} selectedNft={selectedNft}/>
+  <AbstractList 
+    listData={listData} 
+    setSelectedNft={setSelectedNft} 
+  />
+  </>
+    )}
     </div>
-
-    </>
-  );
-}
-
+  )}
 export default App;
